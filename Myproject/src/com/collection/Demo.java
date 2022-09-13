24bc87a5b13e47b042a20aac5a34bb061be4b860
Shubhamrandale <shubhamrandale8@gmail.com>
2022-09-13 19:50:24 +0530

    code for arraylist added

---
+++ Myproject/src/com/collection/Demo.java
@@ -0,0 +1,21 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Demo {
+	
+public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(21);
+		list.add(52);
+		list.add(45);
+		list.add(78);
+		list.add(45);
+		list.add(41);
+		
+		System.out.println(list);
+
+}
+
+}
