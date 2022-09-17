ef0d625ef020d533868792c978a27230f98ddd73
Shubhamrandale <shubhamrandale8@gmail.com>
2022-09-17 15:04:26 +0530

    added new code

---
+++ Myproject/src/com/collection/LS.java
@@ -0,0 +1,16 @@
+package com.collection;
+
+public class LS {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=100; i++) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
